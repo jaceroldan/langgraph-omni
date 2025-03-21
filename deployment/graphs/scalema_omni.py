642ff3af112dc3c6cfeb749238a6fea5a4dee823
jav <joseangelo.bposeats@gmail.com>
2025-03-21 20:12:29 +0800

    [bposeats#3404]  Implement initial functionality for AI-powered project proposal creation

--- deployment/graphs/scalema_omni.py
+++ deployment/graphs/scalema_omni.py
@@ -1,11 +1,10 @@
 # Import general libraries
-from pydantic import BaseModel, Field
+from typing import Literal, TypedDict
 
 # Import Langgraph
 from langgraph.graph import StateGraph, MessagesState, START, END
 from langchain_core.messages import SystemMessage
 from langchain_core.runnables import RunnableConfig
-from langgraph.types import interrupt
 from langgraph.checkpoint.memory import MemorySaver
 
 # Import utility functions
@@ -16,60 +15,34 @@ from utils.models import models
 from graphs.scalema_web3 import scalema_web3_subgraph
 
 
-class HumanQuery(BaseModel):  # Used to structure data
+# Tools
+class ToolCall(TypedDict):
     """
-        This contains the arguments of the User's query that you are chatting with.
-        Always call this tool whenever the User asks anything.
+        Decision on which tool to use
+    """
+    tool_type: Literal["proposal"]
+
+
+def choose_tool(state: MessagesState) -> Literal["scalema_web3_subgraph", END]:
+    """
+        Decide on which tool to use
     """
 
-    query: str = Field(description="This contains the user's query")
-
-
-def should_continue(state: MessagesState):
-    """
-        Decide whether to continue or not
-    """
-
-    messages = state["messages"]
-
-    tool_calls = messages[-1].tool_calls
+    tool_calls = state["messages"][-1].tool_calls
     # If there is no function call, then finish
     if not tool_calls:
         return END
 
-    if tool_calls[0]["name"] == "HumanQuery":
-        return "input_node"
-    else:
-        return END
+    match (tool_calls[0]["args"]["tool_type"]):
+        case "proposal":
+            return "scalema_web3_subgraph"
+        case _:
+            return END
+
+# Schemas
 
 
-def input_node(state: MessagesState):
-    """
-        Facilitates HITL and assists in collecting user input
-    """
-
-    tool_call = state["messages"][-1].tool_calls[0]
-    prompt = tool_call["args"]["query"]
-    formatted_question = (
-        "You asked: {question}, provide an answer for this."
-    )
-
-    user_input = interrupt(formatted_question.format(question=prompt))
-    content_message = (
-        "The user responded with: "
-        "<input>{input}</input>"
-        "\nReview the user's input in relation to the question. Correct it if "
-        "is wrong and provide an explanation."
-    )  # This is temporary until a use case is created
-
-    tool_message = [{
-        "tool_call_id": tool_call["id"],
-        "type": "tool",
-        "content": content_message.format(input=user_input)
-    }]
-    return {"messages": tool_message}
-
-
+# Nodes
 def agent(state: MessagesState, config=RunnableConfig):
     """
         Helps personalizes chatbot messages
@@ -77,33 +50,35 @@ def agent(state: MessagesState, config=RunnableConfig):
 
     # access model name through config passed in the Backend
     model_name = Configuration.from_runnable_config(config).model_name
-    node_model = models[model_name]
+    node_model = models[model_name].bind_tools([ToolCall], parallel_tool_calls=False)
     response = node_model.invoke([SystemMessage(content=MODEL_SYSTEM_MESSAGE)] + state["messages"])
     return {"messages": [response]}
 
 
 # System Messages for the Model
 MODEL_SYSTEM_MESSAGE = (
-    "You are Scalema, a helpful chatbot that helps clients with their business queries. "
-    "If it's your first time talking with a client, be sure to inform them this."
+    "You are Scalema, a helpful chatbot that assists clients with their business queries. "
+    "If this is your first time interacting with a client, introduce yourself and inform them of your role. "
+    "You have the ability to choose the appropriate tools to handle client requests."
+    "\n\nGuidelines for tool usage:"
+    "\n\t- If a user requests assistance with a proposal or provides details for one, "
+    "always call ToolCall with the 'proposal' argument."
+    "\n\t- Do not provide examples unless explicitly asked."
+    "\n\nWhen using tools, do not inform the user that a tool has been called. Instead, "
+    "respond naturally as if the action was performed seamlessly."
 )
 
 
-# Build the graph
+# Initialize Graph
 builder = StateGraph(MessagesState, config_schema=Configuration)
 
 builder.add_node(agent)
-# builder.add_node(input_node)
 builder.add_node("scalema_web3_subgraph", scalema_web3_subgraph)
 
-# builder.add_edge(START, "agent")
-# builder.add_conditional_edges("agent", should_continue)
-# builder.add_edge("input_node", "agent")
 
 builder.add_edge(START, "agent")
-builder.add_edge("agent", "scalema_web3_subgraph")
-builder.add_edge("scalema_web3_subgraph", END)
+builder.add_conditional_edges("agent", choose_tool)
+builder.add_edge("scalema_web3_subgraph", "agent")
 
-# Compile the graph
 checkpointer = MemorySaver()
 graph = builder.compile(checkpointer=checkpointer)
--- deployment/graphs/scalema_web3.py
+++ deployment/graphs/scalema_web3.py
@@ -1,24 +1,111 @@
+# Import general libraries
+from typing import Optional
+from pydantic import BaseModel, Field
+from datetime import datetime
+from uuid import uuid4
 
-from langchain_core.messages import AIMessage
+# Import Langgraph
+from langchain_core.messages import SystemMessage, merge_message_runs
 from langchain_core.runnables import RunnableConfig
-# from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import StateGraph, MessagesState, START, END
+from langgraph.store.base import BaseStore
+from trustcall import create_extractor
 
+# Import utility functions
 from utils.configuration import Configuration
+from utils.trustcall import Spy, extract_tool_info
+from utils.models import models
+
+# Tools
 
 
-def proposal_helper_node(state: MessagesState, config: RunnableConfig):
-    message = AIMessage(content="This is a proposal node.")
+# Schema
+class Project(BaseModel):
+    """
+        This is the schema format for a project.
+    """
+    title: Optional[str] = Field(description="Title of the project. (eg. The Residences at Greenbelt)")
+    project_type: Optional[str] = Field(description="Type of project. (eg. Residential - Condominium)")
+    description: Optional[str] = Field(
+        description="Description of the project itself. This can include the location, amenities, and other details."
+    )
+
+
+# Nodes
+def proposal_helper_node(state: MessagesState, config: RunnableConfig, store: BaseStore):
+    """
+        Handles the tool call from the parent graph.
+    """
+    configurable = Configuration.from_runnable_config(config)
+    model_name = configurable.model_name
+    user_profile_pk = configurable.user_profile_pk
+
+    namespace = ("proposal", user_profile_pk)
+
+    # Retrieve memories from the store
+    existing_items = store.search(namespace)
+    tool_name = "Project"
+    existing_memories = ([(existing_item.key, tool_name, existing_item.value)
+                          for existing_item in existing_items] if existing_items else None)
+
+    TRUSTCALL_FORMATTED_MESSAGE = TRUSTCALL_SYSTEM_MESSAGE.format(time=datetime.now().isoformat())
+    updated_messages = list(
+        merge_message_runs(messages=[SystemMessage(content=TRUSTCALL_FORMATTED_MESSAGE)] + state["messages"][:-1]))
+
+    spy = Spy()
+
+    proposal_extractor = create_extractor(
+        models[model_name],
+        tools=[Project],
+        tool_choice=tool_name,
+        enable_inserts=True
+    ).with_listeners(on_end=spy)
+
+    result = proposal_extractor.invoke({"messages": updated_messages, "existing": existing_memories})
+
+    for r, rmeta in zip(result["responses"], result["response_metadata"]):
+        store.put(
+            namespace,
+            rmeta.get("json_doc_id", str(uuid4())),
+            r.model_dump(mode="json"),
+        )
+
+    # Confirm the tool call made in the parent graph
+    tool_call_id = state["messages"][-1].tool_calls[0]["id"]
+
+    # Extract the changes made by Trustcall and add them to the message.
+    memory_update_msg = extract_tool_info(spy.called_tools, tool_name)
+    message = {"content": memory_update_msg, "role": "tool", "tool_call_id": tool_call_id}
+
     return {"messages": [message]}
 
 
-# Build the graph
+def project_agent(state: MessagesState, config: RunnableConfig):
+    """
+        Handles in summarizing the information from the project schema. Processes that information
+        using the agent and responds accordingly.
+
+        CURRENTLY EMPTY
+    """
+    return {"messages": state["messages"]}
+
+
+TRUSTCALL_SYSTEM_MESSAGE = (
+    "Your only instruction is to reflect on the interaction and call the appropriate tool. "
+    "Always use the provided tool to retain any necessary information. "
+    "Use parallel tool calls to handle updates and insertions simultaneously. "
+    "\nSystem Time: {time}"
+)
+
+
+# Initialize Graph
 subgraph_builder = StateGraph(MessagesState, config_schema=Configuration)
 
 subgraph_builder.add_node("proposal_helper", proposal_helper_node)
+subgraph_builder.add_node("project_agent", project_agent)
 
 subgraph_builder.add_edge(START, "proposal_helper")
-subgraph_builder.add_edge("proposal_helper", END)
+subgraph_builder.add_edge("proposal_helper", "project_agent")
+subgraph_builder.add_edge("project_agent", END)
 
-# checkpointer = MemorySaver()
 scalema_web3_subgraph = subgraph_builder.compile()
