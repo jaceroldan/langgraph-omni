22bb86f6f3e93e7be1b0a4111bb284d8503e9173
jav <joseangelo.bposeats@gmail.com>
2025-03-26 12:54:54 +0800

    Remove redundancy and fix error

--- deployment/graphs/scalema_omni.py
+++ deployment/graphs/scalema_omni.py
@@ -1,28 +1,17 @@
 # Import general libraries
-from pydantic import BaseModel, Field
 
 # Import Langgraph
 from langgraph.graph import StateGraph, MessagesState, START, END
 from langchain_core.messages import SystemMessage
 from langchain_core.runnables import RunnableConfig
-from langgraph.types import interrupt
 from langgraph.checkpoint.memory import MemorySaver
 
 from utils.api_caller import fetch_weekly_task_estimates
-from utils import estimate_tasks_duration
+from utils.utils import estimate_tasks_duration
 
 # Import utility functions
 from utils.configuration import Configuration
 from utils.models import models
-
-
-class HumanQuery(BaseModel):  # Used to structure data
-    """
-        This contains the arguments of the User's query that you are chatting with.
-        Always call this tool whenever the User asks anything.
-    """
-
-    query: str = Field(description="This contains the user's query")
 
 
 def should_continue(state: MessagesState):
@@ -36,39 +25,10 @@ def should_continue(state: MessagesState):
     # If there is no function call, then finish
     if not tool_calls:
         return END
-    # if tool_calls[0]["name"] == "HumanQuery":
-    #     return "input_node"
     if tool_calls[0]["name"] == "fetch_weekly_task_estimates_summary":
         return "fetch_weekly_task_estimates_summary"
     else:
         return END
-
-
-def input_node(state: MessagesState):
-    """
-        Facilitates HITL and assists in collecting user input
-    """
-
-    tool_call = state["messages"][-1].tool_calls[0]
-    prompt = tool_call["args"]["query"]
-    formatted_question = (
-        "You asked: {question}, provide an answer for this."
-    )
-
-    user_input = interrupt(formatted_question.format(question=prompt))
-    content_message = (
-        "The user responded with: "
-        "<input>{input}</input>"
-        "\nReview the user's input in relation to the question. Correct it if "
-        "is wrong and provide an explanation."
-    )  # This is temporary until a use case is created
-
-    tool_message = [{
-        "tool_call_id": tool_call["id"],
-        "type": "tool",
-        "content": content_message.format(input=user_input)
-    }]
-    return {"messages": tool_message}
 
 
 def fetch_weekly_task_estimates_summary(
@@ -158,11 +118,8 @@ builder = StateGraph(MessagesState, config_schema=Configuration)
 builder.add_node(agent)
 builder.add_node(fetch_weekly_task_estimates_summary)
 
-# builder.add_node(input_node)
-
 builder.add_edge(START, "agent")
 builder.add_conditional_edges("agent", should_continue)
-# builder.add_edge("input_node", "agent")
 builder.add_edge("fetch_weekly_task_estimates_summary", "agent")
 
 # Compile the graph
